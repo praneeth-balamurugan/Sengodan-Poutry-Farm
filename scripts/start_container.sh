52d4b0cdb32623dc0909ace0699fc23cdf4c2ca2
Praneeth <praneeth.nkl@gmail.com>
2024-07-19 21:50:50 +0530

    Update Docker image pull command in start_container.sh script

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,6 +2,6 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull docker pull bpraneeth/farm
+docker pull bpraneeth/farm
 # Run the Docker image as a container
 docker run -d -p 5000:5000 bpraneeth/farm
