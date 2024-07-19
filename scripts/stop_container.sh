54be5e2fe730e7e2d62a750056d252096fa891bf
Praneeth <praneeth.nkl@gmail.com>
2024-07-20 00:02:05 +0530

    code clean up

--- scripts/stop_container.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -ex
-
-echo "Stopping existing Docker containers..."
-docker stop $(docker ps -q --filter ancestor=bpraneeth/farm) || true
-docker rm $(docker ps -a -q --filter ancestor=bpraneeth/farm) || true
-echo "Existing Docker containers stopped."
